d7d542a39405c708e06e19c699f5de941eedb146
Valentin Delaye <jonesbusy@gmail.com>
2023-04-08 17:18:45 +0200

    Update

--- code/02-building-a-first-custom-component/src/components/ExpenseItem.js
+++ code/02-building-a-first-custom-component/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expense item!</h2>;
+  return (
+    <div>
+      <div>March 28th 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+      </div>
+      <div>CHF 30</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
