89bb4c29b1c10a180123c205824ffee1a6e34218
Valentin Delaye <jonesbusy@gmail.com>
2023-04-09 07:19:19 +0200

    Update package locks

--- code/05-outputting-dynamic-data/src/components/ExpenseItem.js
+++ code/05-outputting-dynamic-data/src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+  const expenseDate = new Date(2023, 2, 28);
+  const expenseTitle = 'Health Insurance';
+  const expenseAmount = 456.67;
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>{expenseDate.toLocaleDateString()}</div>
       <div className='expense-item__description'>
         <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <div className='expense-item__price'>CHF {expenseAmount}</div>
       </div>
     </div>
   );
